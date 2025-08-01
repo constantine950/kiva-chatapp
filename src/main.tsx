87e8767d7fb030cb42ef852ed822920e0ad5c64c
james <omotoyosiadebeshin@gmail.com>
2025-08-01 23:10:48 +0100

    react query

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { Provider } from "react-redux";
 import store from "./store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,10 +17,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );
