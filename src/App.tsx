0100b158dd7d62f083fbb8eda329f8003b669f69
james <omotoyosiadebeshin@gmail.com>
2025-08-01 22:11:55 +0100

    friend list

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import Settings from "./pages/Settings";
 import ChatDetail from "./pages/ChatDetails";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
-import supabase from "./lib/supabase";
 import AddFriends from "./pages/AddFriends";
+import { syncUserToSupabase } from "./lib/dbqueries";
 
 const router = createBrowserRouter([
   {
@@ -61,36 +61,7 @@ function App() {
   const { user } = useUser();
 
   useEffect(() => {
-    const syncUserToSupabase = async () => {
-      if (!user) return;
-
-      const { data: existingUser, error: selectError } = await supabase
-        .from("Users")
-        .select("*")
-        .eq("clerkId", user.id)
-        .single();
-
-      if (selectError && selectError.code !== "PGRST116") {
-        console.error("Error fetching user:", selectError.message);
-        return;
-      }
-
-      if (!existingUser) {
-        const { error: insertError } = await supabase.from("Users").insert({
-          full_name: user.fullName,
-          email: user.primaryEmailAddress?.emailAddress,
-          username: user.username,
-          clerkId: user.id,
-          image: user.imageUrl,
-        });
-
-        if (insertError) {
-          console.error("Insert error:", insertError.message);
-        }
-      }
-    };
-
-    syncUserToSupabase();
+    syncUserToSupabase(user);
   }, [user]);
 
   return <RouterProvider router={router} />;
---
+++ src/lib/dbqueries.ts
@@ -0,0 +1,53 @@
+import supabase from "./supabase";
+
+type User = {
+  id: string;
+  fullName: string | null;
+  emailAddresses: { emailAddress: string }[];
+  username: string | null;
+  imageUrl: string;
+};
+
+export const syncUserToSupabase = async (user: User | null | undefined) => {
+  if (!user) return;
+
+  const { data: existingUser, error: selectError } = await supabase
+    .from("Users")
+    .select("*")
+    .eq("clerkId", user.id)
+    .single();
+
+  if (selectError && selectError.code !== "PGRST116") {
+    console.error("Error fetching user:", selectError.message);
+    return;
+  }
+
+  if (!existingUser) {
+    const { error: insertError } = await supabase.from("Users").insert({
+      full_name: user.fullName,
+      email: user.emailAddresses[0].emailAddress,
+      username: user.username,
+      clerkId: user.id,
+      image: user.imageUrl,
+    });
+
+    if (insertError) {
+      console.error("Insert error:", insertError.message);
+    }
+  }
+};
+
+export const getRandomUsers = async (user: User) => {
+  const clerkId = user.id;
+
+  const { data, error } = await supabase
+    .from("Users")
+    .select("*")
+    .neq("clerkId", clerkId)
+    .order("RANDOM()");
+
+  if (error) {
+    console.error(error.message);
+  }
+  return data;
+};
